be460e17b678e5942e8bde412ac1f4f33fd149a7
fodys22 <76259989+fodys22@users.noreply.github.com>
2026-05-25 11:03:33 +0200

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,18 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+    
+    if response.status_code == 400:
+        return {
+            'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None,
+            'dominant_emotion': None
+        }
+        
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return {
+        'anger': emotions['anger'], 'disgust': emotions['disgust'], 'fear': emotions['fear'],
+        'joy': emotions['joy'], 'sadness': emotions['sadness'], 'dominant_emotion': dominant_emotion
+    }
